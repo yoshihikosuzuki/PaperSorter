776f1d0f4e5332c0e15289953929e93478c69cac
yoshihikosuzuki <ys.neoteny@gmail.com>
2019-08-18 16:11:39 +0900

    small fix on menu

--- src/main/menu.js
+++ src/main/menu.js
@@ -1,17 +1,48 @@
-import { Menu } from 'electron'
+import { app, Menu } from 'electron'
 
 export default function createMenu(win) {
   const template = [
+    ...(process.platform === 'darwin' ? [{
+      label: app.getName(),
+      submenu: [
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
     {
-      label: "Edit",
+      label: "View",
       submenu: [
         {
           label: "Search",
           accelerator: "CommandOrControl+F",
-          click: () =>  win.webContents.send('search')
-        }
+          click: () => win.webContents.send('search')
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click: () => win.toggleDevTools()
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'togglefullscreen' },
+        ...(process.platform === 'darwin' ? [
+          { type: 'separator' },
+          { role: 'front' },
+          { type: 'separator' },
+          { role: 'window' }
+        ] : [
+            { role: 'close' }
+          ])
       ]
     }
   ]
+
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 }
